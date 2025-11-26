46fdbea8ed360c4b742c4e064c4afb5153bc9670
Jevin Sweval <jevinsweval@gmail.com>
2025-11-26 11:53:05 -0500

    fix tdd example (maybe)

--- crates/oxidd/examples/tdd.rs
+++ crates/oxidd/examples/tdd.rs
@@ -1,21 +1,22 @@
+use oxidd::tdd::new_manager;
 use oxidd::tdd::TDDFunction;
+use oxidd::util::AllocResult;
 use oxidd::ManagerRef;
 use oxidd::TVLFunction;
 use oxidd_core::Manager;
 use oxidd_dump::dot::dump_all;
 
-fn main() {
-    let manager_ref = oxidd::tdd::new_manager(1024, 1024, 1);
-    let (x1, x2, x3, x4) = manager_ref.with_manager_exclusive(|manager| {
-        (
+fn main() -> AllocResult<()> {
+    let manager_ref = new_manager(1024, 1024, 1);
+    let [x1, x2, x3, x4] = manager_ref.with_manager_exclusive(|manager| {
         manager.add_named_vars(["x", "y", "z", "u"]).unwrap();
-        Ok((
-            TDDFunction::var(manager,0).unwrap()?,
-            TDDFunction::var(manager,1).unwrap()?,
-            TDDFunction::var(manager,2).unwrap()?,
-            TDDFunction::var(manager,3).unwrap()?,)
-        )?)
-    });
+        Ok([
+            TDDFunction::var(manager, 0)?,
+            TDDFunction::var(manager, 1)?,
+            TDDFunction::var(manager, 2)?,
+            TDDFunction::var(manager, 3)?,
+        ])
+    })?;
 
     manager_ref.with_manager_shared(|manager| {
         let res = x1.and(&x2).unwrap().or(&x4).unwrap();
@@ -24,5 +25,6 @@ fn main() {
 
         let file = std::fs::File::create("tdd.dot").expect("could not create `tdd.dot`");
         dump_all(file, manager, [(&res, "(x1 ∧ x2) ∨ x4")]).expect("dot export failed");
-    });
+        Ok(())
+    })
 }
