d9539e712bb1f2bd2a8a5bf3be64853df5ced72b
Jevin Sweval <jevinsweval@gmail.com>
2025-11-28 16:50:28 -0500

    oxidd-parser:pla: add broken tests

--- crates/oxidd-parser/src/pla.rs
+++ crates/oxidd-parser/src/pla.rs
@@ -26,3 +26,71 @@ where
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use nom::Finish;
+    use nom::Parser;
+
+    use crate::{util::test::*, Gate};
+
+    use super::*;
+
+    /// PLA tweaked majority-2
+    #[test]
+    fn pla_example() {
+        let input = b".i 4\n\
+            .o 1\n\
+            .ilb A B C D\n\
+            .ob MAJ\n\
+            .p 4\n\
+            11-- 1\n\
+            1-1- 1\n\
+            1--1 1\n\
+            -111 1\n\
+            .e\n";
+
+        let (input, problem) = parse::<()>(&OPTS_NO_ORDER).parse(input).finish().unwrap();
+        assert!(input.is_empty());
+
+        let (circuit, root) = unwrap_problem(problem);
+        let inputs = circuit.inputs();
+        assert_eq!(inputs.len(), 4);
+        assert!(inputs.order().is_none());
+
+        let nodes = &[
+            !v(2), // L -3
+            !v(1), // L -2
+            v(0),  // L 1
+            g(0),  // A 3 2 1 0
+            v(2),  // L 3
+            g(1),  // O 3 2 4 3
+            !v(3), // L -4
+            g(2),  // A 2 6 5
+            v(3),  // L 4
+            g(3),  // A 2 2 8
+            g(4),  // A 2 1 4
+            v(1),  // L 2
+            g(5),  // O 2 2 11 10
+            g(6),  // A 2 12 9
+            g(7),  // O 4 2 13 7
+        ];
+        assert_eq!(root, *nodes.last().unwrap());
+
+        for (i, &gate) in [
+            Gate::and(&[nodes[2], nodes[1], nodes[0]]), // 0: A 3 2 1 0
+            Gate::or(&[nodes[4], nodes[3]]),            // 1: O 3 2 4 3
+            Gate::and(&[nodes[6], nodes[5]]),           // 2: A 2 6 5
+            Gate::and(&[nodes[2], nodes[8]]),           // 3: A 2 2 8
+            Gate::and(&[nodes[1], nodes[4]]),           // 4: A 2 1 4
+            Gate::or(&[nodes[11], nodes[10]]),          // 5: O 2 2 11 10
+            Gate::and(&[nodes[12], nodes[9]]),          // 6: A 2 12 9
+            Gate::or(&[nodes[13], nodes[7]]),           // 7: O 4 2 13 7
+        ]
+        .iter()
+        .enumerate()
+        {
+            assert_eq!(circuit.gate(g(i)), Some(gate), "mismatch for gate {i}");
+        }
+    }
+}
